430a5e029fc7ce7e751063d03ead91a6087b039f
kasp214a <58299656+kasp214a@users.noreply.github.com>
2021-05-28 01:58:39 +0200

    Add files via upload
    Folder and first try at analog to digital conversion on Atmega328

---
+++ Project 3/potential ADC Weight converter.c
@@ -0,0 +1,42 @@
+/*
+ * ADC code.c
+ *
+ * Created: 28/05/2021 00:08:29
+ * Author : kaspe
+ */ 
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+static double Weightvalue = 0;
+static double TargetWeight = 0; 
+
+void setupADC()
+{
+	ADMUX |= (1  << REFS0) | (1 << MUX0) | (1 << MUX2); // set the Voltage reference to be AVCC (REFS0) and Enable ADC5 which is on PC5 
+	ADCSRA |= (1 << ADEN) | (1 << ADIE) | (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2); // ADC Enable (ADEN) make the ADC Interrupt on completions (ADIE), Set Presacler to 128 
+	DIDR0 |= (1 << ADC5D);
+	
+	sei();
+}
+
+void AdcConversion()
+{
+	ADCSRA |= (1 << ADSC); // set's a one to the register that starts the conversion when it's done it'll trigger an interrupt and set this bit to be 0 again to restart just set it to 1 again and it'll start again.
+}
+
+int main(void)
+{
+	setupADC();
+	AdcConversion();
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
+ISR(ADC_vect)
+{
+	Weightvalue=ADC //set's a variable to equal the analog to digital conversion value 
+}
